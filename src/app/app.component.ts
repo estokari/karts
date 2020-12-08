0c496aad6b6886dedaf9b7d6706272b01072dfec
Alberto Corella ortiz <albertocorellaortiz@MacBook-Pro-de-Alberto.local>
2020-12-08 19:13:31 +0100

    Añadimos carrusel al componente principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { DatamanagerService } from './shared/services/datamanager.service';
 import { RaceComponent } from './components/race/race.component';
 import { ChampionshipComponent } from './components/championship/championship.component';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class AppComponent implements OnInit{
+  
   title = 'devaway-app';
 
-  constructor (private datamanagerService: DatamanagerService){
+  constructor (private datamanagerService: DatamanagerService, config: NgbCarouselConfig){
+
+    config.interval = 6000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
 
   }
 
