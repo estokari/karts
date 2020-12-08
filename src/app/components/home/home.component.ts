37423fc33da139973b5ae721167962ef633e7545
Alberto Corella ortiz <albertocorellaortiz@MacBook-Pro-de-Alberto.local>
2020-12-08 23:11:54 +0100

    Creamos controles para navegar por los resultados de la home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RaceComponent } from '../race/race.component';
 import { ChampionshipComponent } from '../championship/championship.component';
+import { NgbCarousel , NgbSlideEvent, NgbSlideEventSource} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [NgbCarousel]
 })
 export class HomeComponent implements OnInit {
 
   races:number = 10;
+  play = true;
+  stop = false;
 
-  constructor() { }
+  @ViewChild('scores', {static : true}) carousel: NgbCarousel;
+
+  constructor(config: NgbCarousel) { 
+    config.interval = 6000;
+    config.keyboard = true;
+    config.pauseOnHover = true;
+  }
 
   ngOnInit(): void {
   }
+
+  goToSlide(slide) {
+    this.carousel.select(slide);
+    this.stopSlider();
+  }
+
+  prevSlider(){
+    this.carousel.prev();
+    this.stopSlider();
+  }
+
+  nextSlider(){
+    this.carousel.next();
+    this.stopSlider();
+  }
+
+  stopSlider(){
+    this.carousel.pause();
+    this.stop = true;
+    this.play = false; 
+  }
+
+  playSlider(){
+    this.carousel.cycle();
+    this.stop = false;
+    this.play = true; 
+  }
+
+
 
 }
